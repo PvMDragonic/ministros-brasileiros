1c2a7cdc2470b8838b061e0326ade090a62b7093
PvMDragonic <dragonicpvm@gmail.com>
2022-11-04 17:42:19 -0300

    Adicionado filtro para datas repetidas.
    Passa por cada elemento, depois de ajeitar as datas, e vê se pessoas
    do mesmo ministério batem numa data igual ou menor do que três anos; se
    bater, assume a data de entrada do maior como data de saída do menor.
    
    Para os que ficaram faltando, simplesmente soma 3 anos à data em que
    assumiu o cargo.

--- ministros.py
+++ ministros.py
@@ -8,66 +8,6 @@ import datetime
 import csv
 import re
 
-def arrumar_datas(sem_duplicatas: list) -> list:
-    def separar_conjunto(sem_duplicatas: list, inicio: int) -> tuple:
-        temp = 0
-        conjunto = []
-        chave_da_vez = None
-        
-        # Separa os elementos semelhantes.
-        for i in range(inicio, len(sem_duplicatas)):
-            temp = i
-
-            if not isinstance(sem_duplicatas[i][-2], datetime.date) and chave_da_vez == None:
-                chave_da_vez = sem_duplicatas[i][0]
-
-            if sem_duplicatas[i][0] != chave_da_vez and chave_da_vez != None:
-                inicio = i
-                return (conjunto, inicio)
-
-            conjunto.append(sem_duplicatas[i])
-        
-        # Caso termine o loop sem dar return.
-        inicio = temp
-        return (conjunto, inicio)
-
-    lista_corrigida = []
-    inicio = 0
-
-    while True:
-        conjunto, inicio = separar_conjunto(sem_duplicatas, inicio)    
-
-        # Trabalha no conjunto de semelhantes.
-        while True:
-            if len(conjunto) == 0:
-                break
-
-            para_deletar = []
-            periodo_selecionado = None
-
-            for elem in conjunto:
-                if periodo_selecionado == None:
-                    periodo_selecionado = elem[-1] + datetime.timedelta(days = 1461) # 4 anos.
-
-                if elem[-1] < periodo_selecionado:
-                    para_deletar.append(elem)
-
-            conjunto = [elem for elem in conjunto if elem not in para_deletar]       
-
-            para_deletar = sorted(para_deletar, key = lambda lista: lista[-1])
-            
-            lista_corrigida.append([
-                para_deletar[0][1],
-                para_deletar[0][2],
-                para_deletar[0][3],
-                para_deletar[-1][-1]
-            ])
-
-        if inicio == len(sem_duplicatas) - 1:
-            break
-
-    return lista_corrigida
-
 def dividir_lista(quantidade, lista):
     return [lista[i::quantidade] for i in range(quantidade)]
 
@@ -194,6 +134,91 @@ def processo(lista, todas_listas):
     for sublista in listas:
         Thread(target = buscar_ministros, args = (sublista, todas_listas, )).start()
 
+def arrumar_datas(sem_duplicatas: list) -> list:
+    def separar_conjunto(sem_duplicatas: list, inicio: int) -> tuple:
+        temp = 0
+        conjunto = []
+        chave_da_vez = None
+        
+        # Separa os elementos semelhantes.
+        for i in range(inicio, len(sem_duplicatas)):
+            temp = i
+
+            if not isinstance(sem_duplicatas[i][-2], datetime.date) and chave_da_vez == None:
+                chave_da_vez = sem_duplicatas[i][0]
+
+            if sem_duplicatas[i][0] != chave_da_vez and chave_da_vez != None:
+                inicio = i
+                return (conjunto, inicio)
+
+            conjunto.append(sem_duplicatas[i])
+        
+        # Caso termine o loop sem dar return.
+        inicio = temp
+        return (conjunto, inicio)
+
+    def corrigir_conjunto(lista_corrigida: list, conjunto: list) -> list:
+        while True:
+            if len(conjunto) == 0:
+                break
+
+            para_deletar = []
+            periodo_selecionado = None
+
+            for elem in conjunto:
+                if periodo_selecionado == None:
+                    periodo_selecionado = elem[-1] + datetime.timedelta(days = 1461) # 4 anos.
+
+                if elem[-1] < periodo_selecionado:
+                    para_deletar.append(elem)
+
+            conjunto = [elem for elem in conjunto if elem not in para_deletar]       
+
+            para_deletar = sorted(para_deletar, key = lambda lista: lista[-1])
+            
+            lista_corrigida.append([
+                para_deletar[0][1],
+                para_deletar[0][2],
+                para_deletar[0][3],
+                para_deletar[-1][-1]
+            ])
+
+        return lista_corrigida
+
+    lista_corrigida = []
+    inicio = 0
+
+    while True:
+        conjunto, inicio = separar_conjunto(sem_duplicatas, inicio)
+        lista_corrigida = corrigir_conjunto(lista_corrigida, conjunto)
+
+        if inicio == len(sem_duplicatas) - 1:
+            break
+        
+    return lista_corrigida
+
+def resolver_datas_iguais(lista: list) -> list:
+    for elem in lista:
+        if elem[-2] != elem[-1]:
+            continue
+
+        for item in lista:
+            # Pula ministérios diferentes.
+            if elem[0] != item[0]:
+                continue
+            # Pula o próprio.
+            if elem == item:
+                continue
+
+            if (item[-2] - elem[-1]) >= datetime.timedelta(days = 1095): # 3 anos.          
+                elem[-1] = item[-2] - datetime.timedelta(days = 1)
+                
+        # Passou batido por todas validações.
+        if elem[-2] == elem[-1]:         
+            elem[-1] = elem[-2] + datetime.timedelta(days = 1095)
+
+    return lista
+
 if __name__ == "__main__":
     # Essa lista vai ficar defasada, com o passar do tempo; TO-DO: fazer isso ficar dinâmico, puxando direto da Wikipédia.
     historico_wikipedia = [
@@ -237,13 +262,14 @@ if __name__ == "__main__":
         if type(elem[-2]) == str:
             sem_duplicatas.remove(elem)
 
+    sem_duplicatas = resolver_datas_iguais(sem_duplicatas)
+
     final = sorted(
         sem_duplicatas, 
-        reverse = True, 
-        key = lambda lista: lista[-2]
+        key = lambda lista: lista[0]
     )
 
-    with open(f'ministros.csv', 'w', newline='', encoding='utf-8') as f:
+    with open(f'ministros_{datetime.datetime.now().date()}.csv', 'w', newline='', encoding='utf-8') as f:
         writer = csv.writer(f)
         for elem in final:
             writer.writerow(elem)
